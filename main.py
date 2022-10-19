25a94c298154e9e2e46429ee80f51b01adde5b6e
Andrea <zorzi.andrea@outlook.com>
2022-10-20 00:44:12 +0200

    Init commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from lib.components.server import Server
+from settings import HOSTNAME, PORT
+
+if __name__ == "__main__":
+    server = Server()
+    print(
+        f"""
+    Server started at http://{HOSTNAME}:{PORT}
+    -----
+    Press CRTL-C to stop
+    """
+    )
+
+    try:
+        server.serve_forever()
+    except KeyboardInterrupt:
+        server.server_close()
+        print("Server stopped")
---
+++ settings.py
@@ -0,0 +1,14 @@
+# Server settings
+
+PORT = 8000
+HOSTNAME = "localhost"
+
+
+URLPATTERNS = {
+    "/": "home.html",
+    "/accesso": "accesso.html",
+    "/registrazione": "registrazione.html",
+    "/dashboard": "dashboard.html",
+    "/errore": "errore.html",
+    "/credenziali_errate": "credenziali_errate.html",
+}
